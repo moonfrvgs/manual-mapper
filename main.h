70fcd4b012e1b72f28740583952e15e03c539382
moonfrvgs <109630432+moonfrvgs@users.noreply.github.com>
2026-02-24 15:14:39 +0000

    Add PEB and LDR_DATA_TABLE_ENTRY structures

--- main.h
+++ main.h
@@ -14,6 +14,7 @@
 #include <stdexcept>
 #include <fstream>
 #include <psapi.h>
+#include <map>
 
 
 
@@ -278,7 +279,7 @@ typedef struct BASE_RELOCATION_ENTRY {
 
 
 FARPROC resolve_address(_In_ const char* function_name) {
-    HMODULE ntdll = GetModuleHandleA("ntdll");
+    HMODULE ntdll = GetModuleHandleA("ntdll.dll");
     if (!ntdll) {
         std::cout << "[-] " << function_name << " is null\n";
         return nullptr;
@@ -308,6 +309,66 @@ struct relocation_information{
 
 
 
+
+typedef struct _RTL_USER_PROCESS_PARAMETERS {
+    BYTE           Reserved1[16];
+    PVOID          Reserved2[10];
+    UNICODE_STRING ImagePathName;
+    UNICODE_STRING CommandLine;
+} RTL_USER_PROCESS_PARAMETERS, * PRTL_USER_PROCESS_PARAMETERS;
+
+typedef struct _PEB_LDR_DATA
+{
+    ULONG Length;                                // +0x00
+    UCHAR Initialized;                           // +0x04
+    PVOID SsHandle;                              // +0x08
+    LIST_ENTRY InLoadOrderModuleList;            // +0x10
+    LIST_ENTRY InMemoryOrderModuleList;          // +0x20
+    LIST_ENTRY InInitializationOrderModuleList;  // +0x30
+
+
+} PEB_LDR_DATA, * PPEB_LDR_DATA;
+typedef void (*PPS_POST_PROCESS_INIT_ROUTINE)(void);
+typedef struct _PEB {
+    BYTE                          Reserved1[2];
+    BYTE                          BeingDebugged;
+    BYTE                          Reserved2[1];
+    PVOID                         Reserved3[2];
+    PPEB_LDR_DATA                 Ldr;
+    PRTL_USER_PROCESS_PARAMETERS  ProcessParameters;
+    PVOID                         Reserved4[3];
+    PVOID                         AtlThunkSListPtr;
+    PVOID                         Reserved5;
+    ULONG                         Reserved6;
+    PVOID                         Reserved7;
+    ULONG                         Reserved8;
+    ULONG                         AtlThunkSListPtr32;
+    PVOID                         Reserved9[45];
+    BYTE                          Reserved10[96];
+    PPS_POST_PROCESS_INIT_ROUTINE PostProcessInitRoutine;
+    BYTE                          Reserved11[128];
+    PVOID                         Reserved12[1];
+    ULONG                         SessionId;
+} PEB, * PPEB;
+
+
+
+typedef struct _LDR_DATA_TABLE_ENTRY
+{
+    LIST_ENTRY InLoadOrderLinks;               // +0x00
+    LIST_ENTRY InMemoryOrderLinks;             // +0x10
+    LIST_ENTRY InInitializationOrderLinks;     // +0x20
+    PVOID DllBase;                             // +0x30
+    PVOID EntryPoint;                          // +0x38
+    ULONG SizeOfImage;                         // +0x40
+    UNICODE_STRING FullDllName;                // +0x48
+    UNICODE_STRING BaseDllName;                // +0x58
+    ULONG Flags;                               // +0x68
+    USHORT LoadCount;                          // +0x6C
+    USHORT TlsIndex;                           // +0x6E
+    LIST_ENTRY HashLinks;                      // +0x70
+    ULONG TimeDateStamp;                       // +0x80
+} LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
 
 
 bool manual_map(_In_ std::string_view process_window_name, _In_ const char* dll_path);
